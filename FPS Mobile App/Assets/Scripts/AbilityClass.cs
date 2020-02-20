27872602fd1a285b5f131d95f342c9b9d7b87cc8
Tommy-Koder <kovathana5608@students.d211.org>
2020-02-20 13:49:04 -0600

    AbilityClass

---
+++ FPS Mobile App/Assets/Scripts/AbilityClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityClass : MonoBehaviour
+{
+    private string abilityName;
+    private float cooldown;
+    private bool isUsed;
+    private float timeTillUse;
+    private string description;
+
+    public AbilityClass(string abilityName, float cooldown, string description)
+    {
+        this.abilityName = abilityName;
+        this.cooldown = cooldown;
+        isUsed = false;
+        timeTillUse = 0;
+        this.description = description;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        timeTillUse++;
+    }
+}

d073d8150cc15192408913f94e7e1dd1ddc9f5f2
Tommy-Koder <kovathana5608@students.d211.org>
2020-02-21 11:17:09 -0600

    Ability Continued

---
+++ FPS Mobile App/Assets/Scripts/AbilityScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityScript : MonoBehaviour
+{
+
+    Rigidbody AbilityBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AbilityBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

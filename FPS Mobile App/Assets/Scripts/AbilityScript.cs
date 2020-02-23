bb577fd8bd017c39809311b610643a194862a4a4
RealDeadrat <54372955+RealDeadrat@users.noreply.github.com>
2020-02-22 23:13:57 -0600

    GravityPool Working
    I made the black hole ability

--- FPS Mobile App/Assets/Scripts/AbilityScript.cs
+++ FPS Mobile App/Assets/Scripts/AbilityScript.cs
@@ -5,17 +5,76 @@ using UnityEngine;
 public class AbilityScript : MonoBehaviour
 {
 
-    Rigidbody AbilityBody;
+    Rigidbody abilityBody;
+    public Rigidbody playerBody;
+    bool isUsed = false;
+    bool inUse = false;
+    Ability gravityPool;
+    public Transform playerTransform;
+
+    private float useTime;
+    private ForceMode forceType = ForceMode.Impulse;
+
+    private Vector3 suckDirection;
+    private float suckConstant;
+    private float suckMagnitude;
 
     // Start is called before the first frame update
     void Start()
     {
-        AbilityBody = GetComponent<Rigidbody>();
+        abilityBody = GetComponent<Rigidbody>();
+        playerBody = GameObject.Find("Player").GetComponent<Rigidbody>();
+        gravityPool = new Ability("Gravity Pool", 5f, "Creates a blackhole, which explodes after a few seconds");
+        useTime = gravityPool.cooldown;
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+        suckDirection = playerTransform.position - transform.position;
+        suckConstant = suckDirection.x * suckDirection.x + suckDirection.y * suckDirection.y + suckDirection.z * suckDirection.z;
+        suckMagnitude = Mathf.Clamp((-1000/suckConstant),-10000,10000);
+        if (isUsed)
+        {
+            useTime = Time.time + gravityPool.cooldown;
+        }
+        if(inUse && Time.time >= useTime)
+        {
+            inUse = false;
+            useTime = gravityPool.cooldown;
+        }
         
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!inUse)
+        {
+            transform.position = playerTransform.position + new Vector3(0,2,0);
+            transform.Rotate(Vector3.up*playerTransform.localRotation.y);
+            abilityBody.useGravity = false;
+            abilityBody.isKinematic = false;
+        }
+        if (isUsed)
+        {
+            abilityBody.AddRelativeForce(0, 3f, 10, forceType);
+            isUsed = false;
+            abilityBody.useGravity = true;
+        }
+        if(inUse && abilityBody.isKinematic)
+        {
+            playerBody.AddForce(suckMagnitude*suckDirection);
+        }
+    }
+
+    public void UseAbility()
+    {
+        isUsed = true;
+        inUse = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        abilityBody.isKinematic = true;
     }
 }

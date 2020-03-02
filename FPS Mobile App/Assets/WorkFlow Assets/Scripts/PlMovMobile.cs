dfec38f737eb9640d57b7b6fbd730b986e375b60
RealDeadrat <54372955+RealDeadrat@users.noreply.github.com>
2020-03-01 19:29:06 -0600

    Broken Joysticks

---
+++ FPS Mobile App/Assets/WorkFlow Assets/Scripts/PlMovMobile.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlMovMobile : MonoBehaviour
+{
+
+    private Rigidbody playerBody;
+
+    private Vector3 inputMov;
+    public float speed;
+
+    public float jumpforce;
+    public ForceMode forceType;
+
+    public LayerMask groundLayers;
+
+    public SphereCollider col;
+
+    private bool didJump;
+    Touch touch;
+
+    Vector2 Mpos;
+    Vector2 relMPos;
+
+    public float cMovx;
+    public float cMovy;
+
+    public RawImage innerCircle;
+    public RawImage outerCircle;
+
+    private int nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
+
+        nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            for(int i=0; i<Input.touchCount;i++)
+            {
+                if(Input.GetTouch(i).position.x < Screen.width / 2)
+                {
+                    touch = Input.GetTouch(i);
+                }
+            }
+            
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Mpos = touch.position;
+                outerCircle.rectTransform.anchoredPosition = Mpos;
+
+                innerCircle.enabled = true;
+                outerCircle.enabled = true;
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                relMPos = (touch.position - Mpos) * Time.deltaTime;
+                //Vector2 relCPos = Vector2.ClampMagnitude(relMPos, 1.0f);
+                innerCircle.rectTransform.anchoredPosition = new Vector2(relMPos.x, relMPos.y)*20f;
+            }
+
+            if (Mathf.Abs(relMPos.x) > .3f || Mathf.Abs(relMPos.y) > .3f)
+            {
+                inputMov = new Vector3(relMPos.x, 0, relMPos.y);
+            }
+
+            if(touch.phase == TouchPhase.Ended)
+            {
+                innerCircle.enabled = false;
+                outerCircle.enabled = false;
+
+                playerBody.velocity = new Vector3(0, playerBody.velocity.y,0);
+                inputMov = Vector3.zero;
+            }
+        }
+
+        if (didJump && IsGrounded())
+        {
+
+            playerBody.AddForce(Vector3.up * jumpforce, forceType);
+            didJump = false;
+        }
+
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+
+    void FixedUpdate()
+    {
+       playerBody.AddRelativeForce(inputMov * speed);
+     
+    }
+
+    public void Jump()
+    {
+        didJump = true;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+}
+
